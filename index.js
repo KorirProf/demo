7805b01697eb4ea4a7e6f6102a8e0cee290d0329
prof korir <dismaskorir19@gmail.com>
2022-04-29 11:36:23 +0300

    my first JS

---
+++ index.js
@@ -0,0 +1,20 @@
+
+//my First js
+console.log('I really love you');
+//if statements
+let age=21;
+if (age>=18){
+    console.log("You can drive")
+}else{
+    console.log("You need to be 18+ in order to drive")
+}
+//operation mathematics
+console.log(18+21)
+let firstName= 'Korir';
+const lastName ='Dismas'; 
+const pi= 3.142;
+console.log(pi)
+console.log(firstName)
+console.log(lastName)
+console.log(!(23>10))
+console.log(!true)
